3aad7053cc433dbed7b997f523c02155bbd78ee0
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-05-31 02:01:05 -0400

    fix: implement 2FA functionality in login test by creating user session and managing cookies

--- exercises/03.authenticated-e2e/01.problem.login/tests/e2e/2fa.test.ts
+++ exercises/03.authenticated-e2e/01.problem.login/tests/e2e/2fa.test.ts
@@ -1,39 +1,30 @@
 import { faker } from '@faker-js/faker'
+import * as setCookieParser from 'set-cookie-parser'
+import { getSessionExpirationDate, sessionKey } from '#app/utils/auth.server.ts'
+import { prisma } from '#app/utils/db.server.ts'
 import { invariant } from '#app/utils/misc.tsx'
+import { sessionStorage } from '#app/utils/session.server.ts'
 import { expect, test } from '#tests/playwright-utils.ts'
-// 🐨 you'll want the set-cookie-parser module to parse
-// 🐨 you'll also need:
-// sessionKey from auth.server.ts
-// prisma from db.server.ts
-// sessionStorage from session.server.ts
-
 test('Users can add 2FA to their account and use it when logging in', async ({
 	page,
 	insertNewUser,
 }) => {
 	const password = faker.internet.password()
-	// 💣 you can remove this comment when you've used the user object.
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const user = await insertNewUser({ password })
 	invariant(user.name, 'User name is not defined')
 
-	// 🐨 create a new session for the user using prisma.session.create
-	// 💰 you can reference the login utility from auth.server.ts if you need a reminder.
+	const session = await prisma.session.create({
+		data: { userId: user.id, expirationDate: getSessionExpirationDate() },
+	})
 
-	// 🐨 get the cookieSession from sessionStorage.getSession
-	// 🐨 set the sessionKey on the cookieSession to the session.id
-	// 🐨 commit the cookieSession using sessionStorage.commitSession
-	// 🐨 parse the setCookie header using setCookieParser.parseString
-	// 💰 const cookieConfig = setCookieParser.parseString(...)
+	const cookieSession = await sessionStorage.getSession()
+	cookieSession.set(sessionKey, session.id)
+	const setCookieHeader = await sessionStorage.commitSession(cookieSession)
+	const cookieConfig = setCookieParser.parseString(setCookieHeader) as any
 
-	// 🐨 add the cookie to the browser context using page.context().addCookies
-	// by passing the cookieConfig to addCookies (note, you need to add the domain!)
-	// so you can use {...cookieConfig, domain: 'localhost'}
+	// throw new Error(JSON.stringify({ cookieConfig }, null, 2))
 
-	// 🦺 The cookieConfig type can't be specific enough to satisfy the
-	// requirements of addCookies. If you'd like to parse the cookieConfig with
-	// zod to make it more type safe, be my guest, but I don't think it's necessary
-	// in this case. Simply add `as any` to the cookieConfig and you'll be good to go.
+	await page.context().addCookies([{ ...cookieConfig, domain: 'localhost' }])
 
 	await page.goto('/settings/profile')
 
