3da6c51cfb2be8640fff422e0592b65bcac666ec
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-09 21:41:46 -0400

    test: refactor user profile tests to implement root loader and honeypot for enhanced routing and data handling

--- exercises/07.remix-component/02.problem.multiple-routes/app/routes/users+/$username.test.tsx
+++ exercises/07.remix-component/02.problem.multiple-routes/app/routes/users+/$username.test.tsx
@@ -7,6 +7,8 @@ import { createRemixStub } from '@remix-run/testing'
 import { render, screen } from '@testing-library/react'
 import { AuthenticityTokenProvider } from 'remix-utils/csrf/react'
 import { test } from 'vitest'
+import { type loader as rootLoader } from '#app/root.tsx'
+import { honeypot } from '#app/utils/honeypot.server.ts'
 import { default as UsernameRoute, type loader } from './$username.tsx'
 
 function createFakeUser() {
@@ -54,24 +56,36 @@ test('The user profile when not logged in as self', async () => {
 test('The user profile when logged in as self', async () => {
 	const user = createFakeUser()
 	const App = createRemixStub([
-		// 🐨 the root route's path should be "/" and it also needs an id of "root"
-		// because our utility for getting the user requires it (check #app/utils/user.ts)
-		// 🐨 for the loader, you can do the same sort of thing we do for the username
-		// route loader, just import the type of the rootLoader and use that.
-		// 🐨 the root loader stub will need to return the same properties the real one does
-		// but you can fake them out. So you may want to check out the root loader
-
-		// 🐨 nest this route inside a root route that provides the root loader's data
-		// which you'll find in app/root.tsx
 		{
-			path: '/users/:username',
-			Component: UsernameRoute,
-			loader(): Awaited<ReturnType<typeof loader>> {
+			id: 'root',
+			path: '/',
+			loader(): Awaited<ReturnType<typeof rootLoader>> {
+				const honeyProps = honeypot.getInputProps()
 				return json({
-					user,
-					userJoinedDisplay: user.createdAt.toLocaleDateString(),
+					username: user.username,
+					user: {
+						...user,
+						roles: [],
+					},
+					theme: 'light',
+					toast: null,
+					ENV: { MODE: 'test' },
+					csrfToken: 'test-csrf-token',
+					honeyProps,
 				})
 			},
+			children: [
+				{
+					path: '/users/:username',
+					Component: UsernameRoute,
+					loader(): Awaited<ReturnType<typeof loader>> {
+						return json({
+							user,
+							userJoinedDisplay: user.createdAt.toLocaleDateString(),
+						})
+					},
+				},
+			],
 		},
 	])
 
