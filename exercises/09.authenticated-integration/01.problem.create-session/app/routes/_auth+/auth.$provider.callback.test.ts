1025b5fd9ef35e5bf9f7173dac0208c70ec78441
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-11 19:26:20 -0400

    test: add cleanup logic in auth callback tests for GitHub and user data to ensure test isolation

--- exercises/09.authenticated-integration/01.problem.create-session/app/routes/_auth+/auth.$provider.callback.test.ts
+++ exercises/09.authenticated-integration/01.problem.create-session/app/routes/_auth+/auth.$provider.callback.test.ts
@@ -1,9 +1,14 @@
 import { faker } from '@faker-js/faker'
 import { http } from 'msw'
 import * as setCookieParser from 'set-cookie-parser'
-import { expect, test } from 'vitest'
+import { afterEach, expect, test } from 'vitest'
+import { getSessionExpirationDate, sessionKey } from '#app/utils/auth.server.ts'
 import { connectionSessionStorage } from '#app/utils/connections.server.ts'
+import { prisma } from '#app/utils/db.server.ts'
 import { invariant } from '#app/utils/misc.tsx'
+import { sessionStorage } from '#app/utils/session.server.ts'
+import { insertedUsers, insertNewUser } from '#tests/db-utils.ts'
+import { deleteGitHubUsers, insertGitHubUser } from '#tests/mocks/github.ts'
 import { server } from '#tests/mocks/index.ts'
 import { consoleError } from '#tests/setup/setup-test-env.ts'
 import { loader } from './auth.$provider.callback.ts'
@@ -12,14 +17,15 @@ const ROUTE_PATH = '/auth/github/callback'
 const PARAMS = { provider: 'github' }
 const BASE_URL = 'https://www.epicstack.dev'
 
-// 🐨 add some cleanup for the github users that are inserted during the tests:
-// use deleteGitHubUsers from '#tests/mocks/github.ts' in an afterEach
-
-// 🐨 add some cleanup for our own users that are inserted during the tests:
-// use insertedUsers from '#tests/db-utils.ts' and make sure to clear it after
-// deleting the users.
-// 💰 if you need a reminder for how we did this in playwright,
-// check tests/playwright-utils.ts
+afterEach(async () => {
+	await deleteGitHubUsers()
+})
+afterEach(async () => {
+	await prisma.user.deleteMany({
+		where: { id: { in: [...insertedUsers] } },
+	})
+	insertedUsers.clear()
+})
 
 test('a new user goes to onboarding', async () => {
 	const request = await setupRequest()
@@ -45,48 +51,58 @@ test('when auth fails, send the user to login with a toast', async () => {
 })
 
 test('when a user is logged in, it creates the connection', async () => {
-	// 🐨 create a new github user with insertGitHubUser from '#tests/mocks/github.ts'
-	// 🐨 create a new user (use our insertNewUser util from '#tests/db-utils.ts')
-	// 🐨 create a new session that's connected to that user
+	const githubUser = await insertGitHubUser()
+	const newUser = await insertNewUser()
+	const session = await prisma.session.create({
+		select: { id: true },
+		data: {
+			userId: newUser.id,
+			expirationDate: getSessionExpirationDate(),
+		},
+	})
 
-	// 🐨 pass the session.id and githubUser.code to the setupRequest function
-	// then go below to handle that
-	const request = await setupRequest()
+	const request = await setupRequest({
+		sessionId: session.id,
+		code: githubUser.code,
+	})
 	const response = await loader({ request, params: PARAMS, context: {} })
 	assertRedirect(response, '/settings/profile/connections')
 	assertToastSent(response)
-
-	// 🐨 look in prisma.connection for the connection that should have been
-	// created for the user.id + the githubUser.profile.id
-	// 🐨 assert the connection exists
+	const connection = await prisma.connection.findFirst({
+		where: {
+			userId: newUser.id,
+			providerId: githubUser.profile.id,
+		},
+	})
+	expect(connection).toBeTruthy()
 })
 
-// 🐨 accept an (optional) sessionId and optional code as an argument here
-async function setupRequest() {
+async function setupRequest({
+	sessionId,
+	code = faker.string.uuid(),
+}: { sessionId?: string; code?: string } = {}) {
 	const url = new URL(ROUTE_PATH, BASE_URL)
 	const state = faker.string.uuid()
-	// 🐨 the code comes in as an argument now, but we should still default the
-	// code to faker.string.uuid()
-	const code = faker.string.uuid()
 	url.searchParams.set('state', state)
 	url.searchParams.set('code', code)
 
 	const connectionSession = await connectionSessionStorage.getSession()
 	connectionSession.set('oauth2:state', state)
 
-	// 🐨 get the cookieSession from sessionStorage (#app/utils/sessions.server.ts)
-
-	// 🐨 if there is a sessionId, then set it into the cookieSession under the
-	// sessionKey property (💰 sessionKey should come from #app/utils/auth.server.ts)
+	const cookieSession = await sessionStorage.getSession()
+	if (sessionId) cookieSession.set(sessionKey, sessionId)
 
 	const connectionSetCookieHeader =
 		await connectionSessionStorage.commitSession(connectionSession)
 	const request = new Request(url.toString(), {
 		method: 'GET',
 		headers: {
-			// 🐨 multiple cookies are joined by the semicolon character,
-			// so you'll need to join both cookie values together with a semicolon here:
-			cookie: convertSetCookieToCookie(connectionSetCookieHeader),
+			cookie: [
+				convertSetCookieToCookie(
+					await sessionStorage.commitSession(cookieSession),
+				),
+				convertSetCookieToCookie(connectionSetCookieHeader),
+			].join(';'),
 		},
 	})
 	return request
