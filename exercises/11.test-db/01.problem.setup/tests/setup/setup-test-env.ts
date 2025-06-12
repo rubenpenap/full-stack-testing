b2e3e7d7ae5db7adfbb5bb281d7f1ba20fe6ca4c
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-12 13:53:50 -0400

    refactor: simplify test environment setup by removing unnecessary user cleanup logic and importing db-setup.ts

--- exercises/11.test-db/01.problem.setup/tests/setup/setup-test-env.ts
+++ exercises/11.test-db/01.problem.setup/tests/setup/setup-test-env.ts
@@ -1,12 +1,10 @@
 import 'dotenv/config'
-// 🐨 import ./db-setup.ts here
+import './db-setup.ts'
 import '#app/utils/env.server.ts'
 import '@testing-library/jest-dom/vitest'
 import { installGlobals } from '@remix-run/node'
 import { cleanup } from '@testing-library/react'
 import { afterEach, beforeEach, vi, type MockInstance } from 'vitest'
-import { prisma } from '#app/utils/db.server.ts'
-import { insertedUsers } from '#tests/db-utils.ts'
 import { server } from '../mocks/index.ts'
 import './custom-matchers.ts'
 
@@ -14,14 +12,6 @@ installGlobals()
 
 afterEach(() => server.resetHandlers())
 afterEach(() => cleanup())
-// 💣 now that we're isolated, we no longer need to worry about keeping track
-// of the users and we're wiping the whole database anyway, so you can remove this:
-afterEach(async () => {
-	await prisma.user.deleteMany({
-		where: { id: { in: [...insertedUsers] } },
-	})
-	insertedUsers.clear()
-})
 
 export let consoleError: MockInstance<typeof console.error>
 
