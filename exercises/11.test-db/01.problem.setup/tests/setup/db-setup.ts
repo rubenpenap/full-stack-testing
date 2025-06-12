ed31b442851b0fe15443047a2c8431c2a0849abe
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-12 13:53:46 -0400

    test: set up database connection and cleanup for test environment using Prisma

--- exercises/11.test-db/01.problem.setup/tests/setup/db-setup.ts
+++ exercises/11.test-db/01.problem.setup/tests/setup/db-setup.ts
@@ -1,14 +1,26 @@
-// 🐨 create a databaseFile variable that points to `./tests/prisma/data.db`
-// 🐨 create a full path to that file with path.join(process.cwd(), databaseFile)
-// 🐨 set process.env.DATABASE_URL to that full path
+import path from 'node:path'
+import { execaCommand } from 'execa'
+import fsExtra from 'fs-extra'
+import { afterAll, afterEach, beforeAll } from 'vitest'
 
-// 🐨 before all the tests run, use execaCommand from 'execa' to run:
-//   prisma migrate reset --force --skip-seed --skip-generate
+const databaseFile = './test/prisma/data.db'
+const fullDatabasePath = path.join(process.cwd(), databaseFile)
+process.env.DATABASE_URL = `file:${fullDatabasePath}`
 
-// 🐨 after each test, dynamically import prisma from #app/utils/db.server.ts and
-// delete all the users from the database
-// 💰 we dynamically import prisma so it's not loaded before the environment
-// variable is set: await import('#app/utils/db.server.ts')
+beforeAll(async () => {
+	await execaCommand(
+		'prisma migrate reset --force --skip-seed --skip-generate',
+		{ stdio: 'inherit' },
+	)
+})
 
-// 🐨 after all the tests are finished, dynamically import prisma again and
-// call prisma.$disconnect(), then delete the databaseFile with fsExtra.remove
+afterEach(async () => {
+	const { prisma } = await import('#app/utils/db.server.ts')
+	await prisma.user.deleteMany()
+})
+
+afterAll(async () => {
+	const { prisma } = await import('#app/utils/db.server.ts')
+	await prisma.$disconnect()
+	await fsExtra.remove(fullDatabasePath)
+})
